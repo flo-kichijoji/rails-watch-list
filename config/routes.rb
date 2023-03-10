ea984984278c639cde629b4ad1972c689ac30299
FLO KICHIJOJI <flokichijoji@outlook.fr>
2023-03-10 13:58:57 +0100

    scaffold article

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles, except: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'lists#index'
+  root to: 'lists#index'
   resources :lists do
-      resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create]
       resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
